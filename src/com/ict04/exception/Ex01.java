dd1eaa88b1141dba5875172b64d80b56ccd6926a
plokigit <yny9720@naver.com>
2021-04-05 16:18:02 +0900

    four_0405

--- src/com/ict04/exception/Ex01.java
+++ src/com/ict04/exception/Ex01.java
@@ -2,8 +2,27 @@ package com.ict04.exception;
 
 public class Ex01 {
 	public static void main(String[] args) {
-		// 예외처리
-		// try ~ catch ~
+		// 예외처리 : Exception
+		// 			try ~ catch ~
+		// 목적 : 비정상적인 종료를 정상적인 종료로 유도,
+		//		  예외 발생 시 예외에 대한 처리를 해준다
+		// 형식 : try{
+		//				예외가 발생 할 수 있는 문장;
+		//				예외가 발생 할 수 있는 문장;
+		//			}catch (예외 발생 객체 e){
+		//				예외 발생 시 처리하는 문장 ;}
 		
+		try {
+			int[] var = {10, 20, 30};
+			for (int i = 0; i <= var.length; i++) {
+				System.out.println(1);
+				System.out.println(2);
+				System.out.println("var[" + i + "] = " + var[i]);
+				System.out.println(3);
+				System.out.println(4);
+			}
+		} catch (Exception e) {
+		}
+		System.out.println("수고하셨습니다.");
 	}
 }
